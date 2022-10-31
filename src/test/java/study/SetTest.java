b1d54f1a1353647868646068e1b0843a6ebad65d
IYS <dbgls1@naver.com>
2022-10-31 18:17:21 +0900

    학습테스트 실습 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  // Test Case 구현
+
+  @Test//요구사항1
+  void size(){
+    //then
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+
+  //요구사항2
+  @ParameterizedTest
+  @ValueSource(ints = {1,2,3})
+  void contains(int number){
+    assertTrue(numbers.contains(number));
+
+  }
+
+    //요구사항3
+  @ParameterizedTest
+  @CsvSource(value = {"1, true", "2, true", "3, true", "4, false", "5, false"})
+  void aa(int input, boolean expected){
+    assertEquals(numbers.contains(input), expected);
+  }
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +13,43 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test //요구사항1
+    void split(){
+        //contain(), containOnly(), containExactly() 차이
+        //split(String), split(String, int) 차이
+
+        String[] actual1 = "1,2".split(",");
+        assertThat(actual1).containsExactly("1", "2");
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).contains("1");
+    }
+    @Test //요구사항2
+    void substring(){
+        //substring(int), substring(int, int) 차이
+
+        String s = "(1,2)";
+        int n = s.length();
+
+        String actual = s.substring(1, n - 1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+    @Test //요구사항3
+    @DisplayName("입력값 범위 밖일 경우 StringIndexBoundsException 확인")
+    void charAt(){
+        //given
+        String s = "abc";
+
+        //when, then
+        assertThatThrownBy( ()-> s.charAt(s.length()) )
+            .isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("index out of range")
+            .hasMessageContaining(String.valueOf(s.length()));
+
+        //혹은
+/*        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> s.charAt(s.length()) )
+            .withMessageContaining("String index out of range")
+            .withMessageContaining(String.valueOf(s.length()));*/
+    }
+
 }
